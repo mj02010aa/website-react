bab5504f3d9f347a8d50130bac30bab249453a28
Eleven <11@apptime.tech>
2021-09-03 14:24:31 +0300

    added files

---
+++ src/hooks/wouter-hash.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect, useCallback } from "react";
+import { Router, Route } from "wouter";
+
+const currentLoc = () => window.location.hash.replace("#", "") || "/";
+
+const useHashLocation = () => {
+  const [loc, setLoc] = useState(currentLoc());
+
+  useEffect(() => {
+    const handler = () => setLoc(currentLoc());
+
+    window.addEventListener("hashchange", handler);
+    return () => window.removeEventListener("hashchange", handler);
+  }, []);
+
+  const navigate = useCallback(to => (window.location.hash = to), []);
+  return [loc, navigate];
+};
+
+export default useHashLocation;
