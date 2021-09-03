8fb439a00805f88b9c2eca95f9ecd988fd252d5d
Eleven <11@apptime.tech>
2021-09-03 14:24:05 +0300

    added files

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  build: {
+    outDir: "build"
+  },
+  server: {
+    strictPort: true,
+    hmr: {
+      port: 443
+    }
+  }
+});
